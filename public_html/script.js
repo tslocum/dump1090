05e67099185a0dd322a45a419db217cbf216d28b
Trevor Slocum <tslocum@gmail.com>
2015-09-25 15:45:05 -0700

    Allow sidebar to shrink
    Remove unnecessary CSS declaration
    
    Fix indentation
    
    Move zoom controls to bottom left
    
    Remove px
    
    Remove duplicate zoom control and move scale control

--- public_html/script.js
+++ public_html/script.js
@@ -417,11 +417,15 @@ function initialize_map() {
 	var mapOptions = {
 		center: new google.maps.LatLng(CenterLat, CenterLon),
 		zoom: ZoomLvl,
+		zoomControl: true,
+		zoomControlOptions: {
+			style: google.maps.ZoomControlStyle.SMALL,
+			position: google.maps.ControlPosition.LEFT_BOTTOM
+		},
+		scaleControl: true,
 		mapTypeId: google.maps.MapTypeId.ROADMAP,
 		mapTypeControl: true,
 		streetViewControl: false,
-                zoomControl: true,
-                scaleControl: true,
 		mapTypeControlOptions: {
 			mapTypeIds: mapTypeIds,
 			position: google.maps.ControlPosition.TOP_LEFT,
